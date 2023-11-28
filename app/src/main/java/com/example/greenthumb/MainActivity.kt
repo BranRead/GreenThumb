0427aec010d94f630f06093e65d8b79d4d8d8964
BranRead <branrtread@gmail.com>
2023-11-28 14:30:05 -0400

    UI Coming along, issue popping up with buttons

--- app/src/main/java/com/example/greenthumb/MainActivity.kt
+++ app/src/main/java/com/example/greenthumb/MainActivity.kt
@@ -1,3 +1,14 @@
+// Garbage can icon
+// <a href="https://www.flaticon.com/free-icons/recycle-bin" title="recycle bin icons">Recycle bin icons created by lakonicon - Flaticon</a>
+// Watering can icon
+// <a href="https://www.flaticon.com/free-icons/watering-can" title="watering can icons">Watering can icons created by Mihimihi - Flaticon</a>
+// Plus icon
+// <a href="https://www.flaticon.com/free-icons/plus" title="plus icons">Plus icons created by dmitri13 - Flaticon</a>
+// Coffee Shop plants
+// Photo by <a href="https://unsplash.com/@ceydaciftci?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ceyda Çiftci</a> on <a href="https://unsplash.com/photos/green-potted-plants-on-brown-wooden-seat-dDVU6D_6T80?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+// Main app background
+// Photo by <a href="https://unsplash.com/@anniespratt?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Annie Spratt</a> on <a href="https://unsplash.com/photos/green-leaf-hX_hf2lPpUU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
 package com.example.greenthumb
 
 import android.os.Bundle
@@ -7,12 +18,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -39,7 +56,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -100,7 +120,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(plants: List<Plant>, plantDao: PlantDao){
     var isTitleScreen by rememberSaveable {mutableStateOf(true)}
-    Surface(modifier = Modifier.fillMaxSize(), color = Color(234, 242, 239, 255)) {
+    Surface(modifier = Modifier.fillMaxSize(), color = Color(246, 245, 250, 255)) {
         if(isTitleScreen) {
             TitleScreen(onContinueClicked = { isTitleScreen = false })
         } else {
@@ -115,36 +135,77 @@ fun TitleScreen(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.titlescreen),
+                contentScale = ContentScale.FillBounds
+            ),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = stringResource(R.string.app_name))
-        Button(
-            modifier = Modifier.padding(vertical = 24.dp),
-            onClick = onContinueClicked
+        Row(
+            modifier = Modifier
+                .background(
+                    Color(144, 103, 198, 200),
+                    RoundedCornerShape(10.dp)
+                )
+                .border(
+                    2.dp,
+                    Color(4, 15, 15, 255),
+                    RoundedCornerShape(10.dp)
+                )
+                .padding(16.dp)
+                .clickable { onContinueClicked() }
         ) {
-            Text("Enter the Jungle...")
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(0.dp, 16.dp)
+            ){
+                Text(text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.headlineLarge
+                        .copy(color = Color(246, 245, 250, 255))
+                )
+            }
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(30.dp)
+                    .padding(0.dp, 16.dp)
+            ){
+                Text(text = "Tap to start!",
+                    color = Color(246, 245, 250, 255))
+            }
+
         }
     }
 }
 
 @Composable
 fun SeparateCards(plants: List<Plant>, plantDao: PlantDao){
-    val plantArray = remember {
-        mutableStateListOf<Plant>()
-    }
-
-    for (plant in plants) {
-        plantArray.add(plant)
-    }
-
-    LazyColumn (modifier = Modifier.padding(vertical = 4.dp)) {
-        items (1){
-            AddNewPlantCard(plantDao, plantArray)
+    Box(modifier = with(Modifier){
+        fillMaxSize()
+        .paint(
+            painterResource(id = R.drawable.mainscreen),
+            contentScale = ContentScale.FillBounds)
+    }) {
+        val plantArray = remember {
+            mutableStateListOf<Plant>()
         }
-        items (items = plantArray) {plant ->
-            PlantCard(plant, plantDao, plantArray)
+
+        for (plant in plants) {
+            plantArray.add(plant)
+        }
+
+        LazyColumn (modifier = Modifier.padding(vertical = 4.dp)) {
+            items (1){
+                AddNewPlantCard(plantDao, plantArray)
+            }
+            items (items = plantArray) {plant ->
+                PlantCard(plant, plantDao, plantArray)
+            }
         }
     }
 }
@@ -154,11 +215,11 @@ fun AddNewPlantCard(plantDao: PlantDao, plantArray: SnapshotStateList<Plant>) {
     Column {
         Card(
             colors = CardDefaults.cardColors(
-                containerColor = Color(102, 119, 97, 255),
+                containerColor = Color(142, 68, 61, 210),
             ),
             modifier = Modifier
                 .padding(vertical = 4.dp, horizontal = 8.dp)
-                .border(2.dp, Color(13, 9, 10, 255), RoundedCornerShape(10.dp))
+                .border(2.dp, Color(4, 15, 15, 255), RoundedCornerShape(10.dp))
         ) {
             CardContentEntry(plantDao, plantArray)
         }
@@ -186,7 +247,8 @@ fun CardContentEntry(plantDao: PlantDao, plantArray: SnapshotStateList<Plant>) {
         ){
             Text(text = "New plant", style = MaterialTheme.typography.headlineLarge.copy(
                 fontWeight = FontWeight.Thin,
-                fontSize = 32.sp
+                fontSize = 32.sp,
+                color = Color(246, 245, 250, 255)
             ))
         }
         Column {
@@ -249,11 +311,11 @@ fun CardContentEntry(plantDao: PlantDao, plantArray: SnapshotStateList<Plant>) {
 fun PlantCard(plant: Plant, plantDao: PlantDao, plantArray: SnapshotStateList<Plant>) {
     Card(
         colors = CardDefaults.cardColors(
-            containerColor = Color(102, 119, 97, 255)
+            containerColor = Color(36, 130, 50, 200)
         ),
         modifier = Modifier
             .padding(vertical = 4.dp, horizontal = 8.dp)
-            .border(2.dp, Color(13, 9, 10, 255), RoundedCornerShape(10.dp))
+            .border(2.dp, Color(144, 103, 198, 255), RoundedCornerShape(10.dp))
     ) {
         CardContent(plant, plantDao, plantArray)
     }
@@ -281,12 +343,33 @@ fun CardContent(plant: Plant, plantDao: PlantDao, plantArray: SnapshotStateList<
         Column (
             modifier = Modifier
                 .weight(1f)
-                .padding(12.dp)
+                .padding(8.dp)
+                .size(200.dp, 50.dp)
         ){
             Text(text = plant.getName(), style = MaterialTheme.typography.headlineLarge.copy(
                 fontWeight = FontWeight.Thin,
-                fontSize = 32.sp
+                fontSize = 30.sp,
+                color = Color(246, 245, 250, 255)
             ))
+        }
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .padding(4.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.End
+        ) {
+            if(plant.getLastWatered() != null) {
+                Text(
+                    text = daysUntilNextWaterTitle(plant, plantDao),
+                    modifier = Modifier.padding(0.dp, 16.dp),
+                    style = MaterialTheme.typography.labelSmall.copy(
+                        fontWeight = FontWeight.Normal,
+                        color = Color(246, 245, 250, 255)
+                    )
+                )
+            }
         }
         Column {
             IconButton(onClick = { expandedCard = !expandedCard }) {
@@ -307,8 +390,12 @@ fun CardContent(plant: Plant, plantDao: PlantDao, plantArray: SnapshotStateList<
             Text(text = plant.getLightReq())
             if(plant.getLastWatered() != null){
                 Text(text = plant.getLastWatered().month.toString() + " " + plant.getLastWatered().dayOfMonth)
-                Text(text = daysUntilNextWater(plant, plantDao))
+                Text(text = daysUntilNextWaterBody(plant, plantDao))
             }
+
+        }
+
+        Row {
             Button(onClick = { deletePlant(plant, plantDao, plantArray) }) {
                 Text(text = "Delete")
             }
@@ -319,7 +406,18 @@ fun CardContent(plant: Plant, plantDao: PlantDao, plantArray: SnapshotStateList<
     }
 }
 
-fun daysUntilNextWater(plant: Plant, plantDao: PlantDao): String {
+fun daysUntilNextWaterTitle(plant: Plant, plantDao: PlantDao): String {
+    val localDate: LocalDate = LocalDate.now()
+    val lastWater = Period.between(plant.getLastWatered(), localDate).days
+    val daysPassed = plant.getWateringCycle() - lastWater;
+    if(daysPassed >= 0){
+        return "" + daysPassed + " day(s)";
+    } else {
+        return "" + daysPassed.absoluteValue + " day(s) ago";
+    }
+}
+
+fun daysUntilNextWaterBody(plant: Plant, plantDao: PlantDao): String {
 
     val localDate: LocalDate = LocalDate.now()
     val lastWater = Period.between(plant.getLastWatered(), localDate).days
